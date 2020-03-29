53bdcf2431cb07be417b8d51805737d71e0bee5d
MilleCodesLN <mille.helene@outlook.fr>
2020-03-29 11:36:48 +0200

    mise en place des fichiers pour les sessions suivantes

---
+++ js/session14.js
@@ -0,0 +1,13 @@
+//La concaténation
+
+//Challenge : créer une variable prenom et l'afficher
+const prenom = 'Hélène';
+console.log(prenom);
+
+//Maintenant afficher : Bonjour [prenom] !
+console.log("Bonjour " + prenom + ' !');
+
+
+//Maintenant afficher : Bonjour [prenom] [nom] !
+const nom = "MilleCodesLN";
+console.log('Bonjour ' + prenom + ' ' + nom + ' !');
---
+++ js/session16.js
@@ -0,0 +1,23 @@
+// LES OPERATEURS
+// "+" addition
+// "-" soustraction
+// "*" multiplication
+// "/" division
+// "%" modulo <=> 5/2 = 2.5 restant 1
+// "**" exponentielle <=> 5**2 = 25
+
+let nombreUn = 8, nombreDeux = 4, result;
+result = nombreUn + nombreDeux;
+console.log(result);//12
+
+result += 2;
+console.log(result);//14
+
+
+result++;
+console.log(result);//15
+
+
+result--;
+console.log(result);//14
+
