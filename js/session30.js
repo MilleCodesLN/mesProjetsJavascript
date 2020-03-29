8601f4f82b52add485a22cb8fd1cdda493801523
MilleCodesLN <mille.helene@outlook.fr>
2020-03-29 16:38:02 +0200

    Session 31

---
+++ js/session30.js
@@ -0,0 +1,9 @@
+// let fonctionAnonyme = function () {
+//     alert('Je suis une fonction anonyme');
+// }
+
+// fonctionAnonyme();
+
+//Une fonction anonyme qui s'auto-exécute
+
+(function () { alert('Je suis une fonction anonyme') })();
