115a439f4760b3e86a387faa39c179f59b907fa2
MilleCodesLN <mille.helene@outlook.fr>
2020-03-29 17:45:22 +0200

    Session 36

---
+++ js/session36.js
@@ -0,0 +1,8 @@
+let age = prompt("Veuillez indiquer votre âge :");
+if (age < 18) {
+    alert("Vous n'êtes pas majeur !");
+} else if (age < 21) {
+    alert("Vous êtes majeur en France !");
+} else {
+    alert("Vous êtes majeur partout, à vous les casinos!")
+}
