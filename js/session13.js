da9eb235e79d0eb540a7583b9d25e330e01c0d90
MilleCodesLN <mille.helene@outlook.fr>
2020-03-29 10:52:17 +0200

    session 13

---
+++ js/session13.js
@@ -0,0 +1,33 @@
+//Les variables
+
+// Var / let
+
+// let à utiliser maintenant
+
+// var : agit plutot comme une variable globale
+// let : agit plutot comme une variable locale
+
+
+
+//Chaînes de caractères (string)
+let prenom = "Hélène";
+let pseudo = 'MilleCodesLN';
+
+console.log(prenom);
+console.log(pseudo);
+
+//Nombre (number)
+
+let age = 35;
+console.log(age);
+
+//Booleen (boolean)
+
+let majorite = true;
+console.log(majorite);
+
+//Constantes (const)
+
+const formation = "cda";
+//formation = "cours js sur Udemy"; //provoque une erreur
+//On ne peut pas modifier une constante
