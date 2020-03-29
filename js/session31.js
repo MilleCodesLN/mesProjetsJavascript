f70fee9ff1e146572ba259d164ac6cf6a5ee733c
MilleCodesLN <mille.helene@outlook.fr>
2020-03-29 17:03:51 +0200

    Session 33

--- js/session31.js
+++ js/session31.js
@@ -2,8 +2,13 @@ function abracadabra() {
     let prenom = prompt("Bonjour ! Pouvez-vous me donner votre prénom?");
     let nom = prompt("Humm... Il me faudrait aussi votre nom, s'il vous plaît!");
     let age = prompt("Ah oui!! il me faut absolument votre âge pour pouvoir continuer...");
+
     alert("Oh... humm...!!!");
-    alert("Sapristi ! On ne m'avait pas prévenu que c'était vous, " + prenom + " ! Euh... Je veux dire... Monsieur le grand magicien " + nom + " ! Cela fait déjà " + age + " ans que vous faites rayonner notre contrée !");
+
+    alert("Sapristi ! On ne m'avait pas prévenu que c'était vous, "
+        + prenom + " ! Euh... Je veux dire... Monsieur le grand magicien "
+        + nom + " ! Cela fait déjà "
+        + age + " ans que vous faites rayonner notre contrée !");
 }
 
 abracadabra();
